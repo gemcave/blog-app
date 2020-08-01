d61b2d7e67a9a1911fda1ddd5d666b2c80608f6a
yerowyn <yerowyn@gmail.com>
2020-08-01 22:56:03 +0600

    add navigation

---
+++ src/page-renderer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {useRouteMatch} from 'react-router-dom'
+
+const generatePage = page => {
+	const component = () => require(`./pages/${page}`).default 
+
+	try {
+		return React.createElement(component())
+	} catch(err) {
+		 console.error(err);
+		 return React.createElement(() => 404)
+	}
+}
+
+const PageRenderer = () => {
+	const {params: { page }} = useRouteMatch();
+
+	return generatePage(page)
+};
+
+export default PageRenderer;
